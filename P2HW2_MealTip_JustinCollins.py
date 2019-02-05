1d4e6155939adbfa514b1fdf65b21c1e08742125
collinsj1125 <47356409+collinsj1125@users.noreply.github.com>
2019-02-05 08:57:21 -0500

    Add files via upload
    First Submission Tip Calculator

---
+++ P2HW2_MealTip_JustinCollins.py
@@ -0,0 +1,18 @@
+#Tip Calculator
+#02/01/2019
+#CTI-110 P2HW2-Meal Tip Calculator
+#Justin Collins
+#
+
+bill = float(input("Enter the amount of the bill: "))
+
+tip15 = bill * .15
+tip18 = bill * .18
+tip20 = bill * .20
+
+print ("The amount you entered is", bill, "dollars")
+print ("15% tip is $", tip15, "dollars" )
+print ("18% tip is $", tip18, "dollars" )
+print ("20% tip is $", tip20, "dollars" )
+
+
