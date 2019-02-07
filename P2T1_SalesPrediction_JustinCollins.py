8cc53882943eb1dc764593b5c03fce1efb95599d
collinsj1125 <47356409+collinsj1125@users.noreply.github.com>
2019-02-07 10:17:21 -0500

    Add files via upload
    First Submission Sales Prediction

---
+++ P2T1_SalesPrediction_JustinCollins.py
@@ -0,0 +1,11 @@
+# Sales Prediction
+# 02/05/2019
+# CTI-110-Sales Prediciton
+# Justin Collins
+#
+
+total_sales = float (input ('Enter the projected sales: '))
+
+profit = total_sales * 0.23
+
+print ('The profit is $', format (profit, ',.2f') ,sep="")
